af1f7c682af9a510a7a95340403e9b9e4628ad7e
amantu07 <claudiom.garcia77@gmail.com>
2023-01-23 23:49:59 -0300

    se modificaron funciones, se añadio validacion de ingreso de datos y se mejoro el sistema de stats y creacion de pj

--- js/script.js
+++ js/script.js
@@ -1,79 +1,101 @@
-//Se definen los objetos de los jugadores y declaran las variables
-let player = {
-    vida: 50,
-    ataque: 10,
-    defensa: 5,
+//Función constructor para crear un personaje
+function Personaje(vida, ataque_min, ataque_max, defensa_min, defensa_max, especial) {
+    this.ataque_min = ataque_min;
+    this.vida = vida;
+    this.ataque_max = ataque_max;
+    this.defensa_min = defensa_min;
+    this.defensa_max = defensa_max;
+    this.especial = especial;
 }
 
-let enemy = {
-    vida: 50,
-    ataque: 10,
-    defensa: 5,
-}
+// Creación de los objetos player y enemy 
+const player = new Personaje(500, 80, 100, 80, 90, 150);
+const enemy = new Personaje(500, 70, 90, 70, 80, 140);
+
+const ATTACK = 0;
+const DEFEND = 1
 let movimiento_player
 let movimiento_pc
 
+const btnJugar = document.getElementById("jugar")
+
+btnJugar.addEventListener('click', iniciarJuego)
 
 //Inicia El Juego
-alert('Bienvenidos a Knights of Atreia')
+function iniciarJuego() {
+    alert('Bienvenidos a Knights of Atreia')
 
-//Inicia un bucle para pedir movimientos
-while (true) {
+    while (player.vida > 0 && enemy.vida > 0) {
+        movimiento_player = parseInt(prompt("Elige tu movimiento: 0 para atacar, 1 para defender"));
+
+        if (movimiento_player !== ATTACK && movimiento_player !== DEFEND) {
+            alert("Movimiento no válido, por favor elige de nuevo.");
+            continue;
+        }
+
+        if (movimiento_player == ATTACK) {
+            player.ataque = calcularValor(player.ataque_min, player.ataque_max);
+            alert("Elegiste atacar con un ataque de " + player.ataque);
+        } 
+        else if (movimiento_player == DEFEND) {
+            player.defensa = calcularValor(player.defensa_min, player.defensa_max);
+            alert("Elegiste defender con una defensa de " + player.defensa);
+        }
+
+        movimiento_pc = getEnemyMove();
+        if (movimiento_pc == ATTACK) {
+            enemy.ataque = calcularValor(enemy.ataque_min, enemy.ataque_max);
+            alert("El enemigo ataca con un ataque de " + enemy.ataque);
+        } 
+        else if (movimiento_pc == DEFEND) {
+            enemy.defensa = calcularValor(enemy.defensa_min, enemy.defensa_max);
+            alert("El enemigo defiende con una defensa de " + enemy.defensa);
+        }
+
+        //Llama a la funcion para calcular daños    
+        calcularResultado(movimiento_player, movimiento_pc);
+
+    }
+
+    //Comprueba que los personajes tengan vida
     if (player.vida <= 0) {
-        alert("Perdiste")
-        break
+        alert("Perdiste");
+    } else if (enemy.vida <= 0) {
+        alert("Ganaste!");
     }
-    else if (enemy.vida <= 0) {
-        alert('Ganaste!')
-        break
-    }
-    else {
-        //Movimiento Jugador
-        movimiento_player = prompt('Elige tu movimiento\nEscribe 0 para atacar, 1 para defender')
-        if (movimiento_player == 0) {
-            alert('Elegiste atacar')
-        }
-        else if (movimiento_player == 1) {
-            alert('Elegiste defender')
-        }
-
-
-        //Movimiento PC
-        movimiento_pc = Math.round(Math.random())
-        if (movimiento_pc == 0) {
-            alert('El enemigo ataca')
-        }
-        else if (movimiento_pc == 1) {
-            alert('El enemigo defiende')
-        }
-    }
-
-//Llama a la funcion para calcular daños    
-calcularResultado(movimiento_player,movimiento_pc)
-
 }
 
+//Calcula el movimiento del enemigo
+function getEnemyMove() {
+    return Math.round(Math.random());
+}
 
 //Funcion para calcular los daños en los ataques
-function calcularResultado(movimiento_player,_movmiento_pc){
-    if (movimiento_player == 0 && movimiento_pc == 0) {
-        player.vida = player.vida - enemy.ataque
-        enemy.vida = enemy.vida - player.ataque
-        alert('Los dos atacan\nTu vida restante: ' + player.vida + '\nVida restante del enemigo: ' + enemy.vida)
-
+function calcularResultado(movimiento_player, movimiento_pc) {
+    if (movimiento_player == ATTACK && movimiento_pc == ATTACK) {
+        player.vida -= enemy.ataque;
+        enemy.vida -= player.ataque;
+        alert("Los dos atacan\nTu vida restante: " + player.vida + "\nVida restante del enemigo: " + enemy.vida);
     }
-    else if (movimiento_player == 1 && movimiento_pc == 1) {
-        alert('Los dos defienden\nTu vida restante: ' + player.vida + '\nVida restante del enemigo: ' + enemy.vida)
+    else if (movimiento_player == DEFEND && movimiento_pc == DEFEND) {
+        alert("Los dos defienden\nTu vida restante: " + player.vida + "\nVida restante del enemigo: " + enemy.vida);
     }
-    else if (movimiento_player == 0 && movimiento_pc == 1) {
-        enemy.vida = enemy.vida - (player.ataque - enemy.defensa)
-        alert('Atacaste, el enemigo defiende\nTu vida restante: ' + player.vida + '\nVida restante del enemigo: ' + enemy.vida)
+    else if (movimiento_player == ATTACK && movimiento_pc == DEFEND) {
+        enemy.vida -= (player.ataque - enemy.defensa);
+        alert("Atacaste, el enemigo defiende\nTu vida restante: " + player.vida + "\nVida restante del enemigo: " + enemy.vida);
     }
-    else if (movimiento_player == 1 && movimiento_pc == 0) {
-        player.vida = player.vida - (enemy.ataque - player.defensa)
+    else if (movimiento_player == DEFEND && movimiento_pc == ATTACK) {
+        player.vida -= (enemy.ataque - player.defensa);
         alert('Defendiste, el enemigo atacó\nTu vida restante: ' + player.vida + '\nVida restante del enemigo: ' + enemy.vida)
     }
 }
+
+//Calcula el valor del ataque o la defensa tomando los valores minimos y maximos de cada personaje
+function calcularValor(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
 
 
 
